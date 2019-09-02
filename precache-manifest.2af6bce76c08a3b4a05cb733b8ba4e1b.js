d4c5f5a59860c9feff08c9bcffc2cc45a69c6faf
Abdullah Sumsum <abdullahsumsum@Abdullahs-MacBook-Pro.local>
2019-09-03 04:57:04 +0500

    Updates

---
+++ precache-manifest.2af6bce76c08a3b4a05cb733b8ba4e1b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "4515349ed8c200161d6f8b47563e4087",
+    "url": "/codio/static/media/codio.4515349e.png"
+  },
+  {
+    "revision": "6af501093f5aee9d4b73",
+    "url": "/codio/static/js/runtime~main.a1187f5d.js"
+  },
+  {
+    "revision": "eb3f48fac144b9ca4913",
+    "url": "/codio/static/js/main.aaae6d32.chunk.js"
+  },
+  {
+    "revision": "2af3540ee3f00bd4317b",
+    "url": "/codio/static/js/2.a3fdcce5.chunk.js"
+  },
+  {
+    "revision": "eb3f48fac144b9ca4913",
+    "url": "/codio/static/css/main.a73538e7.chunk.css"
+  },
+  {
+    "revision": "8916fafddbd4d7ceed5ffea0abf4bd49",
+    "url": "/codio/index.html"
+  }
+];
